2119b392ee251b302f62a24cd48c97f02566a521
RubystarAshe <rubystarashe@gmail.com>
2018-11-25 22:21:41 +0900

    [C] Nuxt 2.2.0 + Electron 3.0.10
    Nuxt 2.3.1 이후 버전은 renderer패치로
     인하여 external client 에서의 server middleware 작동 버그가 있어 electron 환경에서 dev 모드가 제대로 작동하지 않습니다. 따라서 2.2.0 으로 우선 작업을 진행합니다

---
+++ build.js
@@ -0,0 +1,23 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  console.log('creating windows installer')
+  const rootPath = path.join('./')
+
+  return Promise.resolve({
+    appDirectory: path.join(rootPath, 'ElectronNuxt-win32-x64/'),
+    noMsi: true,
+    outputDirectory: path.join(rootPath, 'windows-installer'),
+    loadingGif: path.join(rootPath, 'assets/installing.gif'),
+    exe: 'ElectronNuxt.exe',
+    setupExe: 'example_installer.exe'
+  })
+}
